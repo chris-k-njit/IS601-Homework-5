1d681e634a0896465e060aba8fb4d5ba4c1f740c
chris-k-njit <ck378@njit.edu>
2024-02-29 17:04:19 -0500

    more modulo changes.

--- calculator/commands/modulo_command.py
+++ calculator/commands/modulo_command.py
@@ -2,5 +2,7 @@ from .command_interface import CommandInterface
 
 class Modulo(CommandInterface):
     def execute(self, *args):
+        if args[1] == 0:
+            raise ZeroDivisionError("Cannot divide by zero.")
         return args[0] % args[1]
     
