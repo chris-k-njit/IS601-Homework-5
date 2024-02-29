07991af757fc64f2fe7a7cdc2ed47ba55c6fd788
chris-k-njit <ck378@njit.edu>
2024-02-29 16:03:09 -0500

    comment added.

--- calculator/commands/command_interface.py
+++ calculator/commands/command_interface.py
@@ -1,3 +1,4 @@
+# Make sure that all command classes are implementing an execute method.
 class CommandInterface:
     def execute(self, *args):
         raise NotImplementedError("Command must implement an execute method")
