337774371cd7ee8d4aa3c6c6f4b5a7df6e30c7e4
chris-k-njit <ck378@njit.edu>
2024-02-29 16:57:15 -0500

    adjusted test help.

--- tests/test_help_command.py
+++ tests/test_help_command.py
@@ -4,7 +4,7 @@ def test_help(capsys):
     command = Help()
     command.execute()
     captured = capsys.readouterr()
-    assert "Available commands:" in captured.out
+    assert "The available commands for this calculator are: add, subtract, multiply, divide, exponent, sqrt, modulo, greet, help, goodbye, caffeine, exit" in captured.out
     # Ensure some of the command options are present in the output.
     for cmd in ["add", "subtract", "multiply"]:
         assert cmd in captured.out
